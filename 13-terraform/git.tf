9fff1400320aeba754cea36043e30496148d733c
PavelDolbik92 <138321615+PavelDolbik92@users.noreply.github.com>
2023-09-08 00:06:42 +0300

    Update 13-terraform/git.tf

--- 13-terraform/git.tf
+++ 13-terraform/git.tf
@@ -2,7 +2,7 @@ resource "github_repository_file" "files" {
   for_each = fileset("${path.module}", "*.tf")
 
   repository = "devops-hometasks"
-  branch = "main"
+  branch     = "main"
   file       = "13-terraform/${each.value}"
   content    = file("${path.module}/${each.value}")
 }
